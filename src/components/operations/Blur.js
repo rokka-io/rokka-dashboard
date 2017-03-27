15ba9b9f295925fa98ed87b6aa12b47fe139e029
matteo-piatti <matteo.piatti@liip.ch>
2017-03-27 17:37:24 +0200

    Created Blur operation, added Blur operation into factory.js (#4)

---
+++ src/components/operations/Blur.js
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react'
+import InputRange from '../forms/InputRange.js'
+import FormGroup from '../forms/FormGroup'
+
+class Blur extends Component {
+  isRequired (field) {
+    return this.props.required.indexOf(field) !== -1
+  }
+
+  render () {
+    const { defaults, values, errors = {} } = this.props
+
+    return (
+      <div className="row">
+        <div className="col-md-6">
+          <FormGroup label="Blur Sigma" required={this.isRequired('sigma')} error={errors.sigma}>
+            <InputRange
+              name="sigma"
+              min={defaults.sigma.minimum}
+              max={defaults.sigma.maximum}
+              value={values.sigma}
+              defaultValue={defaults.sigma.default}
+              onChange={this.props.onChange}
+            />
+          </FormGroup>
+        </div>
+        <div className="col-md-6">
+          <FormGroup label="Blur Radius" required={this.isRequired('radius')} error={errors.radius}>
+            <InputRange
+              name="radius"
+              min={defaults.radius.minimum}
+              max={defaults.radius.maximum}
+              value={values.radius}
+              defaultValue={defaults.radius.default}
+              onChange={this.props.onChange}
+            />
+          </FormGroup>
+        </div>
+      </div>
+    )
+  }
+}
+
+Blur.propTypes = {
+  onChange: PropTypes.func,
+  defaults: PropTypes.shape({
+    radius: PropTypes.shape({
+      minimum: PropTypes.number,
+      maximum: PropTypes.number
+    }),
+    sigma: PropTypes.shape({
+      minimum: PropTypes.number,
+      maximum: PropTypes.number
+    })
+  }),
+  values: PropTypes.shape({
+    radius: PropTypes.string,
+    sigma: PropTypes.string
+  }),
+  required: PropTypes.array.isRequired,
+  errors: PropTypes.object
+}
+
+export default Blur

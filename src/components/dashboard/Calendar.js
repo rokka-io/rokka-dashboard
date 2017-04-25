a829e9229aa802c64a8d57ba894c8826c89790a4
matteo-piatti <matteo.piatti@liip.ch>
2017-04-25 15:46:14 +0200

    ROKKA-137: fixed the other styles

--- src/components/dashboard/Calendar.js
+++ src/components/dashboard/Calendar.js
@@ -8,8 +8,8 @@ class Calendar extends PureComponent {
     const {onRangeChange, from, to, calendarRef, dateClick, showCalendar} = this.props
     return (
       <div className="height-smaller flo-r pos-r">
-        <div className="flo-r rka-h2 height-20-px" onClick={dateClick}>
-          <p className="flo-r rka-h2 mt-xs height-20-px click-div">
+        <div className="flo-r rka-h2" onClick={dateClick}>
+          <p className="flo-r rka-h2 mt-xs click-div">
             {from.toDate().toLocaleDateString()}
             <span className="ph-sm">-</span>
             {to.toDate().toLocaleDateString()}

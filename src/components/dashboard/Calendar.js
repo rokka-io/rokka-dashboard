6318218e229c3ff87f1065c4fe4749d9b1534b59
Michael Weibel <michael.weibel@gmail.com>
2017-10-25 07:37:06 +0200

    Initial version of range date picker

--- src/components/dashboard/Calendar.js
+++ src/components/dashboard/Calendar.js
@@ -1,10 +1,89 @@
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
-import { MonthView } from 'react-date-picker'
+import moment from 'moment'
+import { Calendar as CalendarComponent } from 'react-calendar-component'
 import enhanceWithClickOutside from 'react-click-outside'
+import cx from 'classnames'
 
 class Calendar extends PureComponent {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      date: moment(),
+      start: null,
+      end: null,
+      selectedRange: new Set()
+    }
+  }
+
+  onChangeMonth (date) {
+    this.setState({date})
+  }
+
+  onPickDate (date) {
+    let { start, end } = this.state
+    if (start && end) {
+      start = null
+      end = null
+    }
+    if (start === null) {
+      start = date
+    } else {
+      end = date
+    }
+    if (start && end && end.isBefore(start)) {
+      const intermediate = start
+      start = end
+      end = intermediate
+    }
+    this.setState({start, end})
+  }
+
+  onMouseOver (date) {
+    const { start, end } = this.state
+    let selectedRange = null
+
+    if (!start || (start && end) || start.isSame(date)) {
+      return
+    }
+    const daysDiff = date.diff(start, 'days')
+    if (daysDiff < 0) {
+      selectedRange = new Set(Array(Math.abs(daysDiff)).fill().map((_, i) => date.clone().add(i, 'day').format()))
+    } else {
+      selectedRange = new Set(Array(daysDiff).fill().map((_, i) => start.clone().add(i, 'day').format()))
+    }
+    this.setState({selectedRange})
+  }
+
   render () {
+    const today = moment()
+    const { start, end, selectedRange } = this.state
+
+    return (
+      <CalendarComponent
+        onChangeMonth={(date) => this.onChangeMonth(date)}
+        date={this.state.date}
+        onPickDate={(date) => this.onPickDate(date)}
+        weekOffset={1}
+        renderDay={date => {
+          const selected = date.isSame(start) || date.isSame(end)
+          const inRange = start && end && date.isBetween(start, end, 'day')
+          const hover = selectedRange.has(date.format())
+          const className = cx('fill', {
+            'today': date.isSame(today, 'day'),
+            'weekend': date.isoWeekday() >= 6,
+            'selected': selected || inRange || hover
+          })
+          return (
+            <div className={className} onMouseOver={() => this.onMouseOver(date)}>
+              {date.format('D')}
+            </div>
+          )
+        }}
+      />
+    )
+    /*
     const {onRangeChange, from, to, calendarRef, dateClick, showCalendar} = this.props
     return (
       <div className="height-smaller flo-r pos-r">
@@ -33,6 +112,7 @@ class Calendar extends PureComponent {
         ) : null}
       </div>
     )
+    */
   }
 
   handleClickOutside () {
@@ -41,13 +121,15 @@ class Calendar extends PureComponent {
 }
 
 Calendar.propTypes = {
-  onBlurHideCalendar: PropTypes.func.isRequired,
+  onBlurHideCalendar: PropTypes.func.isRequired
+  /*
   onRangeChange: PropTypes.func.isRequired,
   from: PropTypes.object.isRequired,
   to: PropTypes.object.isRequired,
   calendarRef: PropTypes.func.isRequired,
   dateClick: PropTypes.func.isRequired,
   showCalendar: PropTypes.bool.isRequired
+  */
 }
 
 export default enhanceWithClickOutside(Calendar)

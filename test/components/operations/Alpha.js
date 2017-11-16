8fb42fd57fbaf19276b4a2e07ca15d72b46824df
Pedro Couto <couto.pedro.miguel@gmail.com>
2017-11-16 12:22:53 +0100

    added alpha and autorotate with mocked operations

--- test/components/operations/Alpha.js
+++ test/components/operations/Alpha.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import Alpha from '../../../src/components/operations/Alpha'
 import renderer from 'react-test-renderer'
-
-const alphaData = {
-  values: {
-    'mode': 'mask'
-  },
-  defaults: {
-    'mode': {
-      'default': 'mask',
-      'values': [
-        'mask',
-        'remove',
-        'extract'
-      ]
-    }
-  }
-}
+import operations from '../../operations.json'
 
 test('Alpha does render', () => {
   const component = renderer.create(
-    <Alpha values={alphaData.values} defaults={alphaData.defaults} required={[]} />
+    <Alpha values={operations.alpha.properties.mode} defaults={operations.alpha.properties} required={[]} />
   )
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()

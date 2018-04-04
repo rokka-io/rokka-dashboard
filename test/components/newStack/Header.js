662085135738c31818f1ba9c30ec15606c36c33d
Michael Weibel <michael.weibel@gmail.com>
2018-04-04 08:54:55 +0200

    Extract New Stack Header into own component

---
+++ test/components/newStack/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Header from '../../../src/components/newStack/Header'
+import renderer from 'react-test-renderer'
+
+test('Header does render with minimal props', () => {
+  const props = {
+    updatePreview: () => {
+      throw new Error('Should not be called')
+    }
+  }
+  const component = renderer.create(
+    <Header {...props} />
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Header does render with all props set', () => {
+  const props = {
+    updatePreview: () => {
+      throw new Error('Should not be called')
+    },
+    previewImage: {
+      hash: 'foo'
+    },
+    createStackBtnDisabled: true,
+    isPreviewCurrent: true,
+    showLoader: true
+  }
+  const component = renderer.create(
+    <Header {...props} />
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})

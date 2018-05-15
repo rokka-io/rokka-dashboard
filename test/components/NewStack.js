869d7815c24bd3a4065f81ac10340372f5673e61
Pedro Couto <couto.pedro.miguel@gmail.com>
2018-05-15 07:59:06 +0200

    Add Stack Clone test (#65)

--- test/components/NewStack.js
+++ test/components/NewStack.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import renderer from 'react-test-renderer'
+import { DragDropContext } from 'react-dnd'
+import TestBackend from 'react-dnd-test-backend'
 import { NewStack } from '../../src/components/NewStack'
 import operations from '../operations.json'
 import stackOptions from '../stackoptions.json'
+import cloneStack from '../cloneStack.json'
 
 // N.B. this test currently triggers a warning because
 //      onComponentDidMount calls an update on the state.
 //      Making this better testable is one of the goals of refactoring.
+
+function wrapInTestContext (DecoratedComponent) {
+  class TestContextContainer extends Component {
+    render () {
+      return <DecoratedComponent {...this.props} />
+    }
+  }
+
+  return DragDropContext(TestBackend)(TestContextContainer)
+}
 
 test('NewStack does render with minimal props', () => {
   const auth = {
@@ -144,6 +157,38 @@ test('NewStack does render with stacks', () => {
 
   const component = renderer.create(
     <NewStack {...props} />
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('newStack does render with clone stack props', () => {
+  const auth = {
+    organization: 'test-org'
+  }
+  const router = {
+    push: () => {
+      throw new Error('should not be called')
+    }
+  }
+  const stackClone = cloneStack
+  const onOpenChoosePreviewImage = () => {
+    throw new Error('should not be called')
+  }
+  const loadPreviewImage = () => {}
+
+  const props = {
+    auth,
+    operations,
+    stackClone,
+    router,
+    onOpenChoosePreviewImage,
+    loadPreviewImage
+  }
+
+  const WrappedNewStack = wrapInTestContext(NewStack)
+  const component = renderer.create(
+    <WrappedNewStack {...props} />
   )
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()

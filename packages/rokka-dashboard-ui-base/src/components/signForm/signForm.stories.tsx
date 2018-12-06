17eef98471eeb92fa259d650cc11175e564febd6
Michael Weibel <michael.weibel@liip.ch>
2018-12-06 18:24:49 +0100

    HeaderLogo as SVG

--- packages/rokka-dashboard-ui-base/src/components/signForm/signForm.stories.tsx
+++ packages/rokka-dashboard-ui-base/src/components/signForm/signForm.stories.tsx
@@ -6,7 +6,7 @@ import { SignForm } from '../signForm/signForm';
 
 const marketingText = <Heading2 color={colors.tints.white}>Add marketing text here</Heading2>;
 
-storiesOf('Components', module)
+storiesOf('Components / Forms', module)
   // TODO: decide if we want to move that to a separate decorator so it can be used somewhere else, too.
   .addDecorator(story => (
     <div style={{ position: 'relative', width: '100vw', minHeight: '100vh', background: colors.gray.lightest }}>

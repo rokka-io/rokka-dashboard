5c84e5931c97bb7021eb67ca9a03967b202c164d
Michael Weibel <michael.weibel@liip.ch>
2018-12-14 08:33:13 +0100

    feat: add headerNavigation
    There is an issue with the line-height, needs to be fixed

---
+++ packages/rokka-dashboard-ui-base/src/components/headerNavigation/headerNavigation.stories.tsx
@@ -0,0 +1,12 @@
+import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { colors } from '../../identity/colors/colors';
+import { HeaderNavigation } from './headerNavigation';
+
+storiesOf('Components / Header', module)
+  .addDecorator(story => (
+    <div style={{ width: '100vw', height: '64px', padding: '10px', background: colors.brand.primary }}>{story()}</div>
+  ))
+  .add('HeaderNavigation', () => <HeaderNavigation username={text('username', 'rokka')} onLogout={action('onLogout')} />);
---
+++ packages/rokka-dashboard-ui-base/src/components/headerNavigation/headerNavigation.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { Button, LogoutIcon } from '../../elements';
+import { media } from '../../identity/media/media';
+import { spaces } from '../../identity/spaces/spaces';
+import { User } from '../user/user';
+
+interface HeaderNavigationProps {
+  /** Displayed username */
+  username: string;
+  /** Logout callback */
+  onLogout(): void;
+}
+
+export const HeaderNavigation: FunctionComponent<HeaderNavigationProps> = ({ username, onLogout }) => (
+  <StyledHeaderNav>
+    <User username={username} />
+    <Button onClick={onLogout}>
+      <StyledLogoutIcon size="23px" />
+    </Button>
+  </StyledHeaderNav>
+);
+
+const StyledHeaderNav = styled.nav`
+  height: ${spaces.xlarge};
+  line-height: ${spaces.xlarge};
+  float: right;
+
+  ${media.mobile`
+    display: none;
+  `}
+`;
+
+const StyledLogoutIcon = styled(LogoutIcon)`
+  margin-left: ${spaces.medium};
+  float: right;
+  margin-top: 1.25rem; // 20px
+`;

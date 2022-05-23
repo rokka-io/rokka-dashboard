84c64c9362eab67a6ba123a916e50c3e8ed33a8d
Christian Stocker <me@chregu.tv>
2022-05-23 07:31:24 +0200

    update rokka.js

--- src/rokka.js
+++ src/rokka.js
@@ -4,6 +4,7 @@ let client = rokka()
 const max_age = 3600 * 72
 export const ROKKA_DASHBOARD_TOKEN = 'rokka-dashboard-token'
 export const ROKKA_DASHBOARD_ORG = 'rokka-dashboard-org'
+export const ROKKA_DASHBOARD_IPS = 'rokka-dashboard-ips'
 
 export const apiTokenGetCallback = () => {
   return localStorage.getItem(ROKKA_DASHBOARD_TOKEN)
@@ -16,12 +17,17 @@ export function authenticate(apiKey) {
     apiTokenOptions: {
       //no_ip_protection: true, // not sure about this
       expires_in: max_age,
-      renewable: true
+      renewable: true,
+      ips: `request_ip,${localStorage.getItem(ROKKA_DASHBOARD_IPS) || ''}`
     },
     apiTokenRefreshTime: max_age - 3600 * 24, // refresh it after a day
     apiTokenGetCallback: apiTokenGetCallback,
-    apiTokenSetCallback: token => {
+    apiTokenSetCallback: (token, payload) => {
       localStorage.setItem(ROKKA_DASHBOARD_TOKEN, token)
+      if (payload && payload.ips) {
+        // store the ips and remember the last 8 (max is 10 on the API side)
+        localStorage.setItem(ROKKA_DASHBOARD_IPS, payload.ips.slice(0, 8).join(','))
+      }
     }
   })
 }

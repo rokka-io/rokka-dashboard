776323aca1426aa6be39b319c7b3b9d43b24da31
Christian Stocker <me@chregu.tv>
2022-06-02 13:52:16 +0200

    build and deploy here and not on rokka-site anymore

--- buildAndUpload.sh
+++ buildAndUpload.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-aws s3 sync --delete --cache-control "max-age=300, public" --profile rokka --acl public-read  dist/ s3://rokka-dashboard/dashboard/
-aws s3 cp --cache-control "max-age=300, public" --profile rokka --acl public-read  dist/index.html s3://rokka-dashboard/
+export PUBLIC_URL='/dashboard'
 
-aws cloudfront --profile rokka create-invalidation  --distribution-id E389UMLNXZS9QN  --paths '/dashboard/*' > /dev/null
+npm run build
+
+
+
+aws s3 sync --delete --cache-control "max-age=300, public" --profile rokka --acl public-read  build/ s3://rokka-dashboard${PUBLIC_URL}
+aws s3 cp --cache-control "max-age=300, public" --profile rokka --acl public-read  build/index.html s3://rokka-dashboard/
+
+aws cloudfront --profile rokka create-invalidation  --distribution-id E389UMLNXZS9QN  --paths "${PUBLIC_URL}/*" > /dev/null
 
 echo ""
 echo "The site can be reached at: "
-echo "https://rokka.io/dashboard/"
+echo "https://rokka.io${PUBLIC_URL}"
 echo ""

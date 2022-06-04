edf97dd65e51ff95d00ee3ba74e4bc486766381e
Christian Stocker <me@chregu.tv>
2022-06-04 10:02:21 +0200

    add /

--- buildAndUpload.sh
+++ buildAndUpload.sh
@@ -8,7 +8,7 @@ npm run build
 
 
 
-aws s3 sync --delete --cache-control "max-age=300, public" --profile rokka --acl public-read  build/ s3://rokka-dashboard${PUBLIC_URL}
+aws s3 sync --delete --cache-control "max-age=300, public" --profile rokka --acl public-read  build/ s3://rokka-dashboard${PUBLIC_URL}/
 aws s3 cp --cache-control "max-age=300, public" --profile rokka --acl public-read  build/index.html s3://rokka-dashboard/
 
 aws cloudfront --profile rokka create-invalidation  --distribution-id E389UMLNXZS9QN  --paths "${PUBLIC_URL}/*" > /dev/null
